3ada871a2a3183cff2484ef590e29ee9b74b1448
swapman972 <jordan.melidor@hotmail.fr>
2020-04-05 17:16:25 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,34 @@
 class Animal
+    attr_reader :species
+    attr_accessor :nick_name, :weight
+    @@all = []
+    def initialize(nick_name, weight, species)
+        @nick_name =nick_name
+        @weight = weight
+        @species = species
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def zoo
+        ZooAnimal.all.find do |key|
+            if key.animal == self
+                return key.zoo
+            end
+        end
+    end
+    def self.find_by_species(specie)
+        array = []
+        @@all.select do |key|
+            if key.species == specie
+                array << key
+            end
+        end
+        array
+    end
+    def new_zoo(zooo)
+        ZooAnimal.new(zooo, self)
+    end
 
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,59 @@
 class Zoo
+    attr_accessor :name, :location
+    @@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def animals
+        array = []
+        ZooAnimal.all.select do |key|
+            if key.zoo == self
+                array << key.animal
+            end
+        end
+        array
+    end
+    def animal_species
+        array = []
+        ZooAnimal.all.select do |key|
+            if key.zoo == self
+                array << key.animal.species
+            end
+        end
+        array.uniq
+    end
+    def find_by_species(specie)
+        array = []
+        ZooAnimal.all.select do |key|
+            if key.animal.species == specie && key.zoo == self
+                array << key.animal
+            end
+        end
+        array
+    end
+    def animal_nicknames
+        array = []
+        ZooAnimal.all.select do |key|
+            if key.zoo == self
+                array << key.animal.nick_name
+            end
+        end
+        array
+    end
+    def self.find_by_location(location)
+        array = []
+        @@all.select do |key|
+            if key.location == location
+            array << key
+            end
+        end
+        array
+    end
 
 end
